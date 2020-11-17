649efb75dcfa557101ff67eaec99bbe84db69fcc
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-11-17 21:49:11 +0100

    Trying to make the range of buttons show in the exe
    But I get the error that it is out of range, which I for now dont understand.

--- CherryCalc/CherryCalc/MainWindow.xaml.cs
+++ CherryCalc/CherryCalc/MainWindow.xaml.cs
@@ -22,26 +22,80 @@ namespace CherryCalc
     {
         CherryModel cherryModel = new CherryModel();
 
+        Grid Numpad;
+
+
         public MainWindow()
         {
             InitializeComponent();
-            InitialButtons();
+            InitialNumpadButtons();
+            SetupNumpad();
         }
 
-        public void InitialButtons()
+        private void SetupNumpad()
         {
+            Numpad = new Grid();
+            for (int i = 0; i < 5; i++)
+            {
+                Numpad.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Numpad.RowDefinitions.Add(new RowDefinition());
+            }
+
+            foreach (var button in cherryModel._numericalBtns)
+            {
+                Numpad.Children.Add(button);
+            }
+
+            SetButtonPosition();            
+        }
+
+        public void InitialNumpadButtons()
+        {
+            Button doubleZero = new Button();
+            doubleZero.Content = 00;
+            doubleZero.Background = new SolidColorBrush(Color.FromArgb(75, 40, 78, 217));
+            cherryModel._numericalBtns.Add(doubleZero);
+
             for (int i = 0; i < 10; i++)
             {
-                Button button = new Button();
-                cherryModel._numericalBtns.Add(button);
+                Button numButton = new Button();
+                numButton.Content = i;
+                numButton.Background = new SolidColorBrush(Color.FromArgb(75, 40, 78, 217));
+                cherryModel._numericalBtns.Add(numButton);
             }
-            
-            
+
+            Button pointBtn = new Button();
+            pointBtn.Content = ".";
+            pointBtn.Background = new SolidColorBrush(Color.FromArgb(75, 40, 78, 217));
+            cherryModel._numericalBtns.Insert(2, pointBtn);
+
+        }
+
+        public void SetButtonPosition()
+        {
+            var buttonIndex = 0;
+            for (int i = Numpad.RowDefinitions.Count -1; i > 0 ; i--)
+            {
+                for (int j = 0; j < Numpad.ColumnDefinitions.Count -1; j++)
+                {
+                    Grid.SetColumn(Numpad.Children[buttonIndex], j);
+                    Grid.SetRow(Numpad.Children[buttonIndex], i);
+                    buttonIndex++;
+                }
+                
+            }
+
+            MainGrid.Children.Add(Numpad);
+            Grid.SetRow(MainGrid.Children[0], 1);
         }
     }
 
     public class CherryModel
     {
-       public static List<Button> _numericalBtns = new List<Button>();
+       public List<Button> _numericalBtns = new List<Button>();
     }
 }
