3b1731025b17eba11977d78ba3dbe10de2b5884d
Jeffreywolff <Jeffrey.wolff@elev.ga.ntig.se>
2020-11-19 12:36:17 +0100

    Fixed grid and buttons

--- CherryCalc/CherryCalc/MainWindow.xaml.cs
+++ CherryCalc/CherryCalc/MainWindow.xaml.cs
@@ -28,23 +28,25 @@ namespace CherryCalc
         public MainWindow()
         {
             InitializeComponent();
-            InitialNumpadButtons();
+            InitializeNumpadButtons();
+            InitializeOperatorButtons();
             SetupNumpad();
         }
 
         private void SetupNumpad()
         {
             Numpad = new Grid();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 4; i++)
             {
                 Numpad.ColumnDefinitions.Add(new ColumnDefinition());
             }
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 5; i++)
             {
                 Numpad.RowDefinitions.Add(new RowDefinition());
             }
 
+            
             foreach (var button in cherryModel._numericalBtns)
             {
                 Numpad.Children.Add(button);
@@ -53,7 +55,7 @@ namespace CherryCalc
             SetButtonPosition();            
         }
 
-        public void InitialNumpadButtons()
+        public void InitializeNumpadButtons()
         {
             Button doubleZero = new Button();
             doubleZero.Content = "00";
@@ -75,18 +77,50 @@ namespace CherryCalc
 
         }
 
+        public void InitializeOperatorButtons()
+        {
+            var plusBtn = new Button();
+            var minusBtn = new Button();
+            var multiplyBtn = new Button();
+            var divideBtn = new Button();
+            var sqrtBtn = new Button();
+            var clearBtn = new Button();
+            var plusMinusBtn = new Button();
+            var percentBtn = new Button();
+
+            plusBtn.Content = "+";
+            minusBtn.Content = "-";
+            multiplyBtn.Content = "x";
+            divideBtn.Content = "÷";
+            sqrtBtn.Content = "√";
+            clearBtn.Content = "C";
+            plusMinusBtn.Content = "±";
+            percentBtn.Content = "%";
+
+            cherryModel._numericalBtns.Insert(3, plusBtn);
+            cherryModel._numericalBtns.Insert(7, minusBtn);
+            cherryModel._numericalBtns.Insert(11, multiplyBtn);
+            cherryModel._numericalBtns.Insert(15, divideBtn);
+            cherryModel._numericalBtns.Insert(16, clearBtn);
+            cherryModel._numericalBtns.Insert(17, plusMinusBtn);
+            cherryModel._numericalBtns.Insert(18, percentBtn);
+            cherryModel._numericalBtns.Insert(19, sqrtBtn);
+
+        }
+
         public void SetButtonPosition()
         {
             var buttonIndex = 0;
-            for (int i = Numpad.RowDefinitions.Count -1; i > 0 ; i--)
+            for (int i = Numpad.RowDefinitions.Count -1; i >= 0; i--)
             {
-                for (int j = 0; j < Numpad.ColumnDefinitions.Count -1; j++)
+                for (int j = 0; j < Numpad.ColumnDefinitions.Count; j++)
                 {
                     Grid.SetColumn(Numpad.Children[buttonIndex], j);
                     Grid.SetRow(Numpad.Children[buttonIndex], i);
                     buttonIndex++;
                 }
                 
+
             }
 
             MainGrid.Children.Add(Numpad);
